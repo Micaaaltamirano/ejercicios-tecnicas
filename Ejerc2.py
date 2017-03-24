4204d48a7bcac386ac792b656f67942da63d0732
unknown <mikaa.altamirano@gmail.com>
2017-03-24 21:57:38 +0100

    Agrego Ejercicio 2

---
+++ Ejerc2.py
@@ -0,0 +1,11 @@
+# Escribir el algoritmo que, a partir de la cantidad de bancos de un aula y la cantidad de
+# alumnos inscriptos para un curso, permita determinar si alcanzan los bancos existentes.
+# De no ser así informar cuantos bancos sería necesario agregar.
+cant_bancos = 30
+cant_alumnos = int(input("Ingrese la cantidad de alumnos: "))
+if (cant_bancos < cant_alumnos):
+    cant_total = cant_alumnos - cant_bancos
+    print ("Faltan " + str(cant_total) + "bancos.")
+elif (cant_bancos > cant_alumnos):
+    cant_total_2 = cant_bancos - cant_alumnos
+    print ("Sobran " + str(cant_total_2) + " bancos.")
