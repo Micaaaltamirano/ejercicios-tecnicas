1c3f4f7925b20a726a9191c1d97fa960e473f25f
unknown <mikaa.altamirano@gmail.com>
2017-03-24 22:34:46 +0100

    Agrego ejercicio 9

---
+++ Ejerc9.py
@@ -0,0 +1,7 @@
+# Escribir un bucle que muestre los números del 50 al 60 en la consola
+num = 50
+lista_num = []
+while (num <= 60):
+    lista_num.append (num)
+    num += 1
+print ("Los números entre 50 y 60 son: " + str(lista_num))
