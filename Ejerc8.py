2500b46d479ad80fafbdde0257c565ca3bf89529
unknown <mikaa.altamirano@gmail.com>
2017-03-24 22:33:34 +0100

    Agrego ejercicio 8

---
+++ Ejerc8.py
@@ -0,0 +1,7 @@
+# Escribir un bucle que muestre los números del 0 a 50 en la consola
+num = 0
+list_num = []
+while (num <= 50):
+    list_num.append (num)
+    num += 1
+print ("Los números del 0 al 50 son :" + str(list_num))
